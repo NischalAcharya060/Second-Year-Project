27c7a939cfb092bb65e55478e94cddd40e74f968
Nischal Acharya <Nischal060@gmail.com>
2023-03-21 19:22:24 +0545

    making register and login page

--- login.php
+++ login.php
@@ -8,6 +8,43 @@
     <title>Login | Project Management System</title>
 </head>
 <body>
-    
+<div class="container">
+      <div class="box">
+        <h2>Login</h2>
+        <form action="login.php" method="post">
+          <div class="input_box">
+            <label>Username</label>
+            <input type="text" class="name" required />
+          </div>
+		  <div class="input_box">
+            <label>Password</label>
+            <input type="password" class="password" required />
+
+            <div class="password_checkbox">
+              <input type="checkbox" id="show_password" />
+              <label for="show_password">Show Password</label>
+            </div>
+          </div>
+
+          <div class="forgot_password"><a href="#">Forgot Password?</a></div>
+          <button class="login_button" type="submit">Login</button>
+          <div class="signup_link">
+            <a href="register.html">Don't have an account?</a>
+          </div>
+        </form>
+      </div>
+    </div>
+    <script>
+      const passwordInput = document.querySelector(".password");
+      const showPasswordCheckbox = document.querySelector("#show_password");
+
+      showPasswordCheckbox.addEventListener("change", function () {
+        if (this.checked) {
+          passwordInput.type = "text";
+        } else {
+          passwordInput.type = "password";
+        }
+      });
+    </script>
 </body>
 </html>
--- register.php
+++ register.php
@@ -8,6 +8,48 @@
     <title>Register | Project Management System</title>
 </head>
 <body>
-    
+<div class="container">
+      <div class="box">
+        <h2>Sign Up</h2>
+        <form action="register.php" method="POST">
+          <div class="input_box">
+            <label for="username">Username</label>
+            <input type="text" id="username" name="username" required />
+          </div>
+
+          <div class="input_box">
+            <label for="email">Email</label>
+            <input type="email" id="email" name="email" required />
+          </div>
+
+		  <div class="input_box">
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password" required />
+            <div class="password_checkbox">
+              <input type="checkbox" id="show_password" />
+              <label for="show_password">Show Password</label>
+            </div>
+          </div>
+
+          <button class="signup_button" type="submit">Create Account</button>
+
+          <div class="login_link">
+            Already have an account? <a href="login.html">Log in</a>
+          </div>
+        </form>
+      </div>
+    </div>
+    <script>
+      const passwordField = document.getElementById("password");
+      const showPasswordField = document.getElementById("show_password");
+
+      showPasswordField.addEventListener("change", () => {
+        if (showPasswordField.checked) {
+          passwordField.type = "text";
+        } else {
+          passwordField.type = "password";
+        }
+      });
+    </script>
 </body>
 </html>
